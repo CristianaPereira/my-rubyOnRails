0f3ff6dd2104cdd248c4b1b6f1a104f8a77e5ceb
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-16 12:06:50 +0100

    feat: add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   resources :authors
+  # When you use resources, Rails automatically generates routes for the seven default actions that are commonly used in CRUD
+  # index => list all
+  # show => displays one specfic doc
+  # new => form to create a new doc
+  # create => handles form.submit to create a new doc
+  # edit => displays edit form
+  # update => handles form.submit to update
+  # destroy => deletes an doc
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -12,4 +22,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # /about
+  get "about", to: "pages#about"
 end
