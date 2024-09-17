3ce484e19bbda70657cd362dc4b129c62492c846
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-17 14:30:51 +0100

    feat: update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [ :edit, :update ]
   def new
     @user = User.new
   end
@@ -15,9 +15,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+    flash[:notice] = "User was updated successfully."
+    redirect_to @user
+    else
+    render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
+   
     params.require(:user).permit(:username, :email, :password) # :password_confirmation
   end
 end
