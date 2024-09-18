aaf2732955cda6c2c6585f8f91418993e6cd51dd
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-18 09:10:07 +0100

    feat: paginate user.articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [ :edit, :update, :show ]
 
   def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
