56044b6d24408d163328fbf62371bf40d05a5525
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-18 08:56:38 +0100

    feat: login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    # binding.break
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      # flash persist for one request, flash.now is for the we stay on the same page
+      puts "There was something wrong with your login details"
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to home_path
   end
 
 end
